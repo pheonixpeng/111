8e17a0f89cde82e374f197820d63a50059ab5ac2
unknown <1740928596@qq.com>
2024-11-07 21:14:04 +0800

    test: Pass move command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -9,7 +9,24 @@ Executor *Executor::NewExecutor(const Pose &pose) noexcept {
 ExecutorImpl::ExecutorImpl(const Pose &pose) noexcept : pose(pose) {}
 
 // Execute函数
-void ExecutorImpl::Execute(const std::string &commands) noexcept {}
+void ExecutorImpl::Execute(const std::string &commands) noexcept {
+    for (const auto cmd : commands) {
+        if (cmd == 'M') {
+            if (pose.heading == 'E') {
+                ++pose.x;
+
+            } else if (pose.heading == 'W') {
+                --pose.x;
+
+            } else if (pose.heading == 'N') {
+                ++pose.y;
+
+            } else if (pose.heading == 'S') {
+                --pose.y;
+            }
+        }
+    }
+}
 
 // Query函数返回保存的pose
 Pose ExecutorImpl::Query() const noexcept { return pose; }
